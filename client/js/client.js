5a42a87dcceffcac6ba14e5e04e4c569a230eb85
Josh R <up816571@myport.ac.uk>
2020-01-29 21:42:03 +0000

    Added fix for chips label element not working as other label elements within a form

--- client/js/client.js
+++ client/js/client.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 //initialisation for materialize elements
@@ -6,11 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         closeOnClick: false, constrainWidth: false};
     M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), dropdownOptions);
 
-    let issueChipsElem = document.getElementById('tags-list-issue');
-    let issueChipOptions = {data:[{tag: 'Test'}, {tag: 'Tag 2'}], placeholder:"Tags"};
-    M.Chips.init(issueChipsElem, issueChipOptions);
+    let issueChipOptions = {data:[{tag: 'Test'}, {tag: 'Tag 2'}]};
+    M.Chips.init(document.getElementById('tags-list-issue'), issueChipOptions);
+    // M.Chips.init(document.getElementById('tags-list-issue'));
 
-    let developerChipsElem = document.getElementById('tags-list-developer');
     //rewrite inner functions later
     let developerChipOptions = {data:[{tag: 'Test'}, {tag: 'Tag 2'}], onChipAdd: () => {
             let instance = M.Dropdown.getInstance(document.getElementById('dropdown-button'));
@@ -19,12 +19,26 @@ document.addEventListener('DOMContentLoaded', function() {
             let instance = M.Dropdown.getInstance(document.getElementById('dropdown-button'));
             instance.recalculateDimensions();
         }};
-    M.Chips.init(developerChipsElem, developerChipOptions);
+    M.Chips.init(document.getElementById('tags-list-developer'), developerChipOptions);
 
     let modalOptions = {inDuration:100, outDuration:100, startingTop:'5%' , endingTop:'15%' };
     M.Modal.init(document.querySelectorAll('.modal'), modalOptions);
-
     M.CharacterCounter.init(document.querySelectorAll('.has-character-counter'));
     M.FormSelect.init(document.querySelectorAll('select'));
-});
 
+    //Code needed to get chips label to behave as other labels do
+    if (document.querySelector('.modified .chips .chip') != null) {
+        document.querySelector('.modified .chips-label').classList.add('active');
+    }
+
+    document.querySelector('.modified .chips .input').addEventListener('focus', function () {
+        document.querySelector('.modified .chips-label').classList.add('active', 'focus');
+    });
+
+    document.querySelector('.modified .chips .input').addEventListener('blur', function() {
+        if (document.querySelector('.modified .chips .chip') === null  && this.value.length === 0) {
+            document.querySelector('.modified .chips-label').classList.remove('active');
+        }
+        document.querySelector('.modified .chips-label').classList.remove('focus');
+    });
+});
