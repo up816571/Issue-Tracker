84a2ef52edf981a72360b6068bc781e2ac56efe2
Josh R <up816571@myport.ac.uk>
2020-02-02 20:29:26 +0000

    Updated Database schema to include teams and issue priorty

--- db_init.sql
+++ db_init.sql
@@ -2,6 +2,11 @@ drop database if exists issue_tracker;
 create database if not exists issue_tracker;
 use issue_tracker;
 
+CREATE TABLE teams (
+    team_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    team_name varchar(100)
+);
+
 -- Assignment type
 -- Can either be auto or suggested
 CREATE TABLE assignment_type (
@@ -19,7 +24,9 @@ CREATE TABLE users (
   user_name varchar(50) NOT NULL,
   user_assignment_type smallint,
   user_free_time int,
-  FOREIGN KEY (user_assignment_type) REFERENCES assignment_type(assignment_type_id)
+  user_team int,
+  FOREIGN KEY (user_assignment_type) REFERENCES assignment_type(assignment_type_id),
+  FOREIGN KEY (user_team) REFERENCES teams(team_id)
 );
 
 -- State of issues
@@ -34,6 +41,17 @@ INSERT INTO state_type VALUES(2, "In Dev");
 INSERT INTO state_type VALUES(3, "In QA");
 INSERT INTO state_type VALUES(4, "Closed");
 
+-- Issue priority can be Low, Medium, High, or Critical
+CREATE TABLE priority (
+    priority_id smallint PRIMARY KEY NOT NULL,
+    state_name varchar(10)
+);
+
+INSERT INTO priority VALUES(1, "Low");
+INSERT INTO priority VALUES(2, "Medium");
+INSERT INTO priority VALUES(3, "High");
+INSERT INTO priority VALUES(4, "Critical");
+
 -- Issues themselves
 CREATE TABLE issues (
   issue_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -42,16 +60,22 @@ CREATE TABLE issues (
   issue_state smallint NOT NULL,
   issue_completion_time int,
   issue_created_at datetime,
-  user_assigned_id int NOT NULL,
+  issue_priority smallint,
+  user_assigned_id int,
+  team_assigned_id int,
   FOREIGN KEY (user_assigned_id) REFERENCES users(user_id),
-  FOREIGN KEY (issue_state) REFERENCES state_type(state_id)
+  FOREIGN KEY (issue_state) REFERENCES state_type(state_id),
+  FOREIGN KEY (issue_priority) REFERENCES priority(priority_id),
+  FOREIGN KEY (team_assigned_id) REFERENCES teams(team_id)
 );
 
+-- Tags created
 CREATE TABLE tags (
   tag_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   tag_name varchar(50) NOT NULL
 );
 
+-- Link table between tags and users
 CREATE TABLE user_tags (
   u_id int,
   t_id int,
@@ -59,6 +83,7 @@ CREATE TABLE user_tags (
   FOREIGN KEY (t_id) REFERENCES tags(tag_id)
 );
 
+-- Link table between tags and issues
 CREATE TABLE issue_tags (
   i_id int,
   t_id int,
@@ -68,19 +93,19 @@ CREATE TABLE issue_tags (
 
 -- Test data
 
-INSERT INTO users VALUES(NULL, "Test", 1, 0);
-INSERT INTO users VALUES(NULL, "User 2", 2, NULL);
+INSERT INTO users VALUES(NULL, "Test", 1, 0, NULL);
+INSERT INTO users VALUES(NULL, "User 2", 2, NULL, NULL);
 
-INSERT INTO issues VALUES(null, "Backlog 1", "Desc", 1, 5, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "Backlog 2", "Desc", 1, 3, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "Backlog 3", Null, 1, 12, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "In Dev 1", "Desc", 2, 4, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "In Dev 2", "Yeet", 2, 7, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "In QA 1", "", 3, 1, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "In QA 2", "Desc", 3, 2, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "Closed 1", "", 4, 7, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "Closed 2", "Desc", 4, 2, SYSDATE(), 1);
-INSERT INTO issues VALUES(null, "Closed 3", NULL, 4, 3, SYSDATE(), 1);
+INSERT INTO issues VALUES(null, "Backlog 1", "Desc", 1, 5, SYSDATE(), 1, 1, NULL);
+INSERT INTO issues VALUES(null, "Backlog 2", "Desc", 1, 3, SYSDATE(), 2, 1, NULL);
+INSERT INTO issues VALUES(null, "Backlog 3", Null, 1, 12, SYSDATE(), 3, 1, NULL);
+INSERT INTO issues VALUES(null, "In Dev 1", "Desc", 2, 4, SYSDATE(), 4, 1, NULL);
+INSERT INTO issues VALUES(null, "In Dev 2", "Something else", 2, 7, SYSDATE(), 1, 1, NULL);
+INSERT INTO issues VALUES(null, "In QA 1", "", 3, 1, SYSDATE(), 1, 1, NULL);
+INSERT INTO issues VALUES(null, "In QA 2", "Desc", 3, 2, SYSDATE(), 1, 1, NULL);
+INSERT INTO issues VALUES(null, "Closed 1", "", 4, 7, SYSDATE(), 2, 1, NULL);
+INSERT INTO issues VALUES(null, "Closed 2", "Desc", 4, 2, SYSDATE(), 3, 1, NULL);
+INSERT INTO issues VALUES(null, "Closed 3", NULL, 4, 3, SYSDATE(), 1, 1, NULL);
 
 INSERT INTO tags VALUES(NULL, "Java");
 INSERT INTO tags VALUES(NULL, "C");
